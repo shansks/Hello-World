38b367e1d65ce9159a447ae03274d4913f70951b
jinlin.feng <jinlin.feng@kosun-info.com>
2019-03-19 19:22:10 +0700

    代码解析完成，添加每行注释

--- kNN.py
+++ kNN.py
@@ -7,18 +7,18 @@ def createDataSet():
     return group,labels
 
 def classify0(inX,dataSet,labels,k):
-    #距离计算
+    #距离计算,使用欧式公式
     dataSetSize = dataSet.shape[0]    #numpy库的shanpe[0],获取矩阵的第一维的长度,dataSetSize=4
     diffMat = tile(inX,(dataSetSize,1)) - dataSet  #tile([0,0],(4,1)),列方向上重复一次，行方向重复4次
     sqDiffMat = diffMat ** 2    #2次方
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances =sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort()
+    sqDistances = sqDiffMat.sum(axis=1)   #按照欧式公式进行相加
+    distances =sqDistances ** 0.5    #按欧式公式进行求根[1.4866... 1.414....  0    0.1]
+    sortedDistIndicies = distances.argsort()  #对原队列排序，并提取对应的index[2 3 1 0]
     classCount={}
     #选择距离最小的k个点
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    #排序
-    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    #排序,items返回可遍历的（键，值）的元组数组,operator.itemgetter(1)根据第二个域进行排序，reverse=True表示降序，False表示正序
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
--- test.py
+++ test.py
@@ -1,8 +1,36 @@
 import kNN
 from numpy import *
 import operator
+import time
 
 group,labels = kNN.createDataSet()
 print(group)
 print(labels)
+testdata = tile([0,0],(4,1)) - group
+print(group)
 print(group ** 2)
+testdata2 = group ** 2
+testdata3 = sum(testdata2,axis=1)
+print(testdata3)
+testdata4 =testdata3 ** 0.5
+print(testdata4)
+testdata5 = testdata4.argsort()
+print(testdata5)
+classCount = {}
+print('***********')
+for i in range(3):
+    print(i)
+    print(testdata5[i])
+    voteIlabel = labels[testdata5[i]]
+    print(voteIlabel)
+    classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    print(classCount[voteIlabel])
+    print('*********')
+print(classCount)
+print('--------')
+print(classCount.items())
+print(operator.itemgetter(1))
+sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+print('--------')
+print(sortedClassCount)
+print(sortedClassCount[0][0])
